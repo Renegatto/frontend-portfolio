b1863e96b8c13323f086458eae3c070d55b0fae2
Renegatto <edittorr@mail.ru>
2025-03-05 19:11:47 +0300

    Display picked options

--- src/app/components/select-many/index.tsx
+++ src/app/components/select-many/index.tsx
@@ -5,11 +5,24 @@ import clsx from "clsx";
 export const SelectManyExample: FC = () =>
   <SelectMany
     options={[
+      <SelectBar key={-1} selectedElements={[
+        <Selected key={0}>{<>Crazyyyyy</>}</Selected>,
+        <Selected key={1}>{<>Frog</>}</Selected>,
+      ]}/>,
       <Item key={0} selected={false}>{<>Option 1</>}</Item>,
       <Item key={1} selected={true}>{<>Option 2</>}</Item>,
       <Item key={2} selected={false}>{<>Option 3</>}</Item>,
     ]}
   />
+
+const Selected: FC<{children: ReactElement}> = ({children}) =>
+  <div className={styles['select-many__bar__selected']}>{children}</div>
+
+const SelectBar: FC<{selectedElements: ReactElement[]}> = ({selectedElements}) =>
+  <div className={styles['select-many__bar']}>
+    {selectedElements}
+  </div>
+
 const Item: FC<{children: ReactElement, selected: boolean}> = ({children,selected}) =>
   <div className={
     clsx(styles['select-many__item'], selected ? styles['select-many__item__selected'] : undefined)
