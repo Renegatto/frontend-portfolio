afbd94615b365c13e4872d477eac8880a2ac4361
Renegatto <edittorr@mail.ru>
2025-03-08 00:58:43 +0300

    Use flowershop endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import styles from "./page.module.css";
-import { FlowerShopFrontPage } from "./examples/flower-shop/page";
+//import styles from "./page.module.css";
+import { FlowerShopFrontPage } from "./examples/flower-shop";
 
 export default function Home() {
   return (
